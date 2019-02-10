fb727c584e5172ba146dacb4a29ce63fa1e4a00a
princymishra <princymishra1997.5@gmail.com>
2019-02-10 13:52:13 +0530

    Finished jobs model

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class job(models.Model):
+    image = models.ImageField(upload_to='images/') #imagefield is same as text field
+    summary = models.CharField(max_length=200)# summary for details of job
+    #migrate command in cmd is used to make the db know that these are the fields of schema
+    
+
