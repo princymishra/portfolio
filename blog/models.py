717b1ec6890be3935212e7d69f5191e575f2beb6
princymishra <princymishra1997.5@gmail.com>
2019-02-10 14:59:42 +0530

    Finished blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class blog(models.Model):
+    title = models.CharField(max_length=255)
+    pubdate = models.DateField()
+    body  = models.TextField()
+    image = models.ImageField(upload_to = 'images/')
